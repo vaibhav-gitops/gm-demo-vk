c847ea631bf3f3cc8a0089078f874f30cef5ad32
Vaibhav Khunger <vaibhavkhunger@Vaibhavs-Laptop.local>
2025-01-01 16:15:13 +0000

    making some terraform changes

--- lambda/lambda-api-gateway/core-infra/main.tf
+++ lambda/lambda-api-gateway/core-infra/main.tf
@@ -69,3 +69,19 @@ resource "aws_apigatewayv2_route" "test_route" {
   api_id    = aws_apigatewayv2_api.api.id
   route_key = "GET /test"
 }
+
+################################################################################
+# Outputs
+################################################################################
+
+output "api_id" {
+  value = aws_apigatewayv2_api.api.id
+}
+
+output "test_route_id" {
+  value = aws_apigatewayv2_route.test_route.id
+}
+
+output "lambda_exec_role_arn" {
+  value = aws_iam_role.lambda_exec.arn
+}
